dafbee0c1c89137366874538fbb13954184a9ce1
Arjun Saini <arjunsaini.ssn@gmail.com>
2023-07-25 13:33:02 +0530

    github insta link added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,10 +28,10 @@ function Contact() {
                     <img src='https://i.postimg.cc/5yFh8psq/317750-linkedin-icon-min.png' alt='..' /><a href="https://www.linkedin.com/in/arjun-saini007">LinkedIn</a>
                 </div>
                 <div>
-                    <img src='https://i.postimg.cc/5yQD79yY/1-3-min.png' alt='..' /><a href="https://github.com/your-github-id">GitHub</a>
+                    <img src='https://i.postimg.cc/5yQD79yY/1-3-min.png' alt='..' /><a href="https://github.com/ArjunSaini9058">GitHub</a>
                 </div>
                 <div>
-                    <img src='https://i.postimg.cc/hjjkm1q0/1-2-min.png' alt='..' /><a href="https://www.instagram.com/your-instagram-id">Instagram</a>
+                    <img src='https://i.postimg.cc/hjjkm1q0/1-2-min.png' alt='..' /><a href="https://www.instagram.com/arjun_saiini/">Instagram</a>
                 </div>
             </div>
         </div>
